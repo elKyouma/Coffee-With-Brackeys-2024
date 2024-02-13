20fc4f571abe3084322372a5bfaaedf98734739f
Dawid Hebda <dawhebda@gmail.com>
2024-02-13 19:45:31 +0100

    Revert "stealing scenes"
    This reverts commit 7d511d6eef277e55ff68c9a4c5ab504b787774ff.

--- Assets/Scripts/Interactables/Magnifier.cs
+++ Assets/Scripts/Interactables/Magnifier.cs
@@ -10,20 +10,22 @@ public class Magnifier : Item, IInteractable
     public Transform camera1;
     public Transform player;
     private bool distortion = true;
-    private bool inUse = false;
 
 
     public void Update()
     {
-        if (distortion)
-        {
-
-        }
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 toPlayer = player.position - transform.position;
         float dot = Vector3.Dot(forward.normalized, toPlayer.normalized);
 
-        if ((dot > 0 && camera1.localEulerAngles.x < 180) || (dot < 0 && camera1.localEulerAngles.x > 180))
+        Debug.Log(dot);
+        Debug.Log(camera1.localEulerAngles);
+        if (dot > 0 && camera1.localEulerAngles.x < 180)
+        {
+            camera1.Rotate(180, 0, 0);
+            camera1.position.Set(camera1.position.x, camera1.position.y * -1, camera1.position.z);
+        }
+        if (dot < 0 && camera1.localEulerAngles.x > 180)
         {
             camera1.Rotate(180, 0, 0);
             camera1.position.Set(camera1.position.x, camera1.position.y * -1, camera1.position.z);
@@ -32,16 +34,6 @@ public class Magnifier : Item, IInteractable
 
     public override void UseItem()
     {
-        if (!inUse)
-        {
-            Debug.Log("Using " + name);
-
-            inUse = true;
-        }
-        else
-        {
-            inUse = false;
-        }
-
+        Debug.Log("Using " + name);
     }
 }
