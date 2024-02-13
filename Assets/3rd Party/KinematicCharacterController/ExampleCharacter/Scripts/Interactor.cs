6aeca38bdb5c982a691e6fc8efe20b9e699667f9
kapiw04 <wojtowicz.kacper@gmail.com>
2024-02-13 18:23:24 +0100

    Interaction distance

--- Assets/3rd Party/KinematicCharacterController/ExampleCharacter/Scripts/Interactor.cs
+++ Assets/3rd Party/KinematicCharacterController/ExampleCharacter/Scripts/Interactor.cs
@@ -7,7 +7,8 @@ using UnityEngine.InputSystem;
 public class Interactor : MonoBehaviour
 {
     [SerializeField] private float Threshold = 0.97f;
-    
+    [SerializeField] private float InteractionDistance = 3f;
+
     private Camera playerCamera;
     private static List<Transform> interactables;
     private Transform previousSelection = null;
@@ -26,42 +27,43 @@ public class Interactor : MonoBehaviour
     private void Awake()
     {
         playerCamera = GetComponentInChildren<Camera>();
-        interactables = new List<Transform>(); 
+        interactables = new List<Transform>();
     }
 
     Transform FindObjectClosestToCursor()
     {
         Transform result = null;
 
-        float closest = 0f; 
+        float closest = 0f;
         for (int i = 0; i < interactables.Count; i++)
         {
+            if (Vector3.Distance(interactables[i].position, playerCamera.transform.position) > InteractionDistance)
+                continue;
             Vector3 vector1 = playerCamera.transform.forward;
             Vector3 vector2 = interactables[i].position - playerCamera.transform.position;
 
             float lookPercentage = Vector3.Dot(vector1.normalized, vector2.normalized);
-
             if (lookPercentage > closest && lookPercentage >= Threshold)
             {
                 closest = lookPercentage;
                 result = interactables[i];
             }
-
         }
         return result;
     }
 
     Transform FindObjectViaRayCast()
     {
+
         Vector2 screenCentre = new Vector2(Screen.width / 2, Screen.height / 2);
         Ray ray;
-        if(GameManager.Instance.IsInPuzzleMode)
+        if (GameManager.Instance.IsInPuzzleMode)
             ray = playerCamera.ScreenPointToRay(Player.MousePosition);
         else
             ray = playerCamera.ScreenPointToRay(screenCentre);
 
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
+        if (Physics.Raycast(ray, out hit) && Vector3.Distance(playerCamera.transform.position, hit.point) <= InteractionDistance)
         {
             var selection = hit.transform;
             if (selection.GetComponent<IInteractable>() != null)
