7d511d6eef277e55ff68c9a4c5ab504b787774ff
Dawid Hebda <dawhebda@gmail.com>
2024-02-13 19:37:25 +0100

    stealing scenes

--- Assets/Scripts/Interactables/Magnifier.cs
+++ Assets/Scripts/Interactables/Magnifier.cs
@@ -10,22 +10,20 @@ public class Magnifier : Item, IInteractable
     public Transform camera1;
     public Transform player;
     private bool distortion = true;
+    private bool inUse = false;
 
 
     public void Update()
     {
+        if (distortion)
+        {
+
+        }
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 toPlayer = player.position - transform.position;
         float dot = Vector3.Dot(forward.normalized, toPlayer.normalized);
 
-        Debug.Log(dot);
-        Debug.Log(camera1.localEulerAngles);
-        if (dot > 0 && camera1.localEulerAngles.x < 180)
-        {
-            camera1.Rotate(180, 0, 0);
-            camera1.position.Set(camera1.position.x, camera1.position.y * -1, camera1.position.z);
-        }
-        if (dot < 0 && camera1.localEulerAngles.x > 180)
+        if ((dot > 0 && camera1.localEulerAngles.x < 180) || (dot < 0 && camera1.localEulerAngles.x > 180))
         {
             camera1.Rotate(180, 0, 0);
             camera1.position.Set(camera1.position.x, camera1.position.y * -1, camera1.position.z);
@@ -34,6 +32,16 @@ public class Magnifier : Item, IInteractable
 
     public override void UseItem()
     {
-        Debug.Log("Using " + name);
+        if (!inUse)
+        {
+            Debug.Log("Using " + name);
+
+            inUse = true;
+        }
+        else
+        {
+            inUse = false;
+        }
+
     }
 }
