5d459d1cb96e078fb8b50338a336c567a232f861
Adrian24026 <adrian2402glizniewicz@gmail.com>
2024-02-15 22:22:54 +0100

    Added sofa

---
+++ Assets/BoringItem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoringItem : Item
+{
+    public override void UseItem()
+    {
+        //I do nothing XD
+    }
+}
