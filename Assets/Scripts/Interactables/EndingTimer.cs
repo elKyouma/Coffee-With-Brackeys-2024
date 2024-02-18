e4b1bc92a1a2df3dba9057aaa675ef451591cb00
kapiw04 <wojtowicz.kacper@gmail.com>
2024-02-18 11:14:41 +0100

    unable to endwhen lost

--- Assets/Scripts/Interactables/EndingTimer.cs
+++ Assets/Scripts/Interactables/EndingTimer.cs
@@ -7,6 +7,7 @@ public class EndingTimer : OutlineInteractable
     [SerializeField] private SoundSO counterSound;
     [SerializeField] private SoundSO rocketLaunchSound;
     [SerializeField] private AudioSource counterAudioSource;
+    private bool lost;
     public void OnEnable()
     {
         StartCoroutine(EndGame());
@@ -18,11 +19,13 @@ public class EndingTimer : OutlineInteractable
         counterAudioSource.Play();
         yield return new WaitForSeconds(counterSound.audio.length);
         // End game
+        lost = true;
         counterAudioSource.clip = rocketLaunchSound.audio;
         counterAudioSource.Play();
     }
     public override void Interact()
     {
+        if (lost) return;
         counterAudioSource.mute = true;
         StopCoroutine(EndGame());
     }
